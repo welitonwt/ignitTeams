5a24fb507ca717c397b96b4de33568e7fbd8aaad
weliton <weliton.ww@hotmail.com>
2024-02-08 19:31:31 -0300

    star theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  return <Groups />
+  return (
+  <ThemeProvider theme={ theme }>
+    <Groups />
+  </ThemeProvider>
+
+  )
 
 }
 
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  background: black;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+` 
+
+export const Title = styled.Text`
+  color: white;
+  font-size: 32px
+`
