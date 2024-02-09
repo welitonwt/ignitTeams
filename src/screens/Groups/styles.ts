0a724a89912700e557fa6b604f2c618ed1b3e213
weliton <weliton.ww@hotmail.com>
2024-02-09 17:07:19 -0300

    Header start

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  background: ${({ theme }:any ) => theme.COLORS.GRAY_600};
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  background: ${({ theme } ) => theme.COLORS.GRAY_600};
+  padding: 24px;
 ` 
