aaa27eba95a5caf167344bb264d258ab7c2b0158
weliton <weliton.ww@hotmail.com>
2024-02-09 16:46:58 -0300

    statusBar change

--- App.tsx
+++ App.tsx
@@ -1,16 +1,24 @@
-import { Groups } from './src/screens/Groups';
-
-import Theme from './src/Theme';
-import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import { ThemeProvider } from 'styled-components';
+import Theme from './src/Theme';
+
+import { Groups } from './src/screens/Groups';
+
 import { Loading } from './src/components/Loading'
+
+import { StatusBar } from 'react-native'
 
 export default function App() {
 const [fontsLoaded] =  useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
   <ThemeProvider theme={ Theme }>
+    <StatusBar
+      barStyle={'light-content'}
+      backgroundColor='transparent'
+      translucent
+    />
    { fontsLoaded ? <Groups /> : <Loading /> }
   </ThemeProvider>
 
