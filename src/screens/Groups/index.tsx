3812e66e1c0a3693cb2c4ce1bd66c08a6eb0e72d
weliton <weliton.ww@hotmail.com>
2024-02-11 15:54:01 -0300

    component highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container >
       <Header />
+      <Highlight
+        title='Turmas'
+        subtitle='Jogue com a sua Turma'
+      />
     </Container>
   );
 }
