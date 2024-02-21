b81fa516b246a9d63ed3658ce7f61809d3d0c11c
weliton <weliton.ww@hotmail.com>
2024-02-21 13:49:32 -0300

    safeAreaView

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import styled from 'styled-components/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }: any) => theme.COLORS.GRAY_600};
   padding: 24px;
